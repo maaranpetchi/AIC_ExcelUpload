34108ed5a463936fb6fde6cbd3a142039c6ffc1c
Petchiappan.P <Petchiappan.P@KTP-KIT008>
2024-06-10 18:07:40 +0530

    first commit

---
+++ src/app.controller.ts
@@ -0,0 +1,42 @@
+import { Controller, HttpException, HttpStatus, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { AppService } from './app.service';
+import { FileInterceptor } from '@nestjs/platform-express/multer';
+import * as ExcelJS from 'exceljs';
+
+@Controller('excel')
+export class AppController {
+  constructor(private readonly appService: AppService) { }
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadFile(@UploadedFile() file: Express.Multer.File) {
+    try {
+      const workbook = new ExcelJS.Workbook();
+      await workbook.xlsx.load(file.buffer);
+
+      const sheets = workbook.worksheets;
+
+      console.log(sheets, "Sheets");
+
+      // Loop through each sheet
+      sheets.forEach((sheet) => {
+        console.log(`Sheet name: ${sheet.name}`);
+
+        // Read the first column (PageId) from the first sheet
+        if (sheet.name === 'All Pages') {
+          const pageIds = [];
+          for (let row = 3; row <= sheet.rowCount; row++) {
+            const cell = sheet.getCell(`C${row}`);
+            pageIds.push(cell.value);
+          }
+          console.log(`PageIds: ${pageIds}`); 
+        }
+      });
+
+      // Return a success response
+      return { message: 'Excel file uploaded successfully' };
+    } catch (error) {
+      console.error(error);
+      throw new HttpException('Internal server error', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+}
---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: "localhost",
+      port: 5432,
+      username: 'postgres',
+      password: 'password@1',
+      database: 'postgres',
+      entities: [],
+      synchronize: false,
+      ssl:false
+    }),
+    TypeOrmModule.forFeature([]), // Add other entities as necessary
+
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {
+
+}
